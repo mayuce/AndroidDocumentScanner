ddc66661f4d52b7cf9fb70e5049f7e4f56d9081c
soljic95 <marko.soljic@gmail.com>
2019-11-14 15:02:49 +0100

    removed invert button, added automatic binarization after cropping

--- documentscanner/src/main/java/team/clevel/documentscanner/ImageCropActivity.java
+++ documentscanner/src/main/java/team/clevel/documentscanner/ImageCropActivity.java
@@ -12,17 +12,27 @@ package team.clevel.documentscanner;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContextWrapper;
-import android.graphics.*;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
@@ -31,80 +41,79 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import team.clevel.documentscanner.helpers.ScannerConstants;
 import team.clevel.documentscanner.libraries.NativeClass;
 import team.clevel.documentscanner.libraries.PolygonView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 public class ImageCropActivity extends Activity {
 
     private FrameLayout holderImageCrop;
-    private ImageView ivRotate,ivInvert,ivRebase;
+    private ImageView ivRotate, ivInvert, ivRebase;
     private ImageView imageView;
     private PolygonView polygonView;
-    private Bitmap selectedImageBitmap,tempBitmapOrginal;
-    private Button btnImageCrop,btnClose;
+    private Bitmap selectedImageBitmap, tempBitmapOrginal;
+    private Button btnImageCrop, btnClose;
     private NativeClass nativeClass;
-    private boolean isInverted;
     private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_crop);
-        isInverted=false;
-        if (ScannerConstants.selectedImageBitmap!=null)
+        if (ScannerConstants.selectedImageBitmap != null)
             initializeElement();
-        else
-        {
-            Toast.makeText(this,ScannerConstants.imageError,Toast.LENGTH_LONG).show();
+        else {
+            Toast.makeText(this, ScannerConstants.imageError, Toast.LENGTH_LONG).show();
             finish();
         }
     }
 
-    private void setImageRotation()
-    {
-        Bitmap tempBitmap= ScannerConstants.selectedImageBitmap.copy(ScannerConstants.selectedImageBitmap.getConfig(),true);
+    private void setImageRotation() {
+        Bitmap tempBitmap = ScannerConstants.selectedImageBitmap.copy(ScannerConstants.selectedImageBitmap.getConfig(), true);
         for (int i = 1; i <= 4; i++) {
             MatOfPoint2f point2f = nativeClass.getPoint(tempBitmap);
-            if (point2f==null)
-            {
-                tempBitmap= rotateBitmap(tempBitmap,90*i);
-            }
-            else
-            {
-                ScannerConstants.selectedImageBitmap=tempBitmap.copy(ScannerConstants.selectedImageBitmap.getConfig(),true);
+            if (point2f == null) {
+                tempBitmap = rotateBitmap(tempBitmap, 90 * i);
+            } else {
+                ScannerConstants.selectedImageBitmap = tempBitmap.copy(ScannerConstants.selectedImageBitmap.getConfig(), true);
                 break;
             }
         }
     }
 
-    public static Bitmap rotateBitmap(Bitmap source, float angle)
-    {
+    public static Bitmap rotateBitmap(Bitmap source, float angle) {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
-    private void setProgressBar(boolean isShow)
-    {
-        RelativeLayout rlContainer=findViewById(R.id.rlContainer);
-        setViewInterract(rlContainer,!isShow);
+    private void setProgressBar(boolean isShow) {
+        RelativeLayout rlContainer = findViewById(R.id.rlContainer);
+        setViewInterract(rlContainer, !isShow);
         if (isShow)
             progressBar.setVisibility(View.VISIBLE);
         else
             progressBar.setVisibility(View.GONE);
     }
 
-    private void setViewInterract(View view,boolean canDo)
-    {
+    private void setViewInterract(View view, boolean canDo) {
         view.setEnabled(canDo);
         if (view instanceof ViewGroup) {
             for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
-                setViewInterract(((ViewGroup) view).getChildAt(i),canDo);
+                setViewInterract(((ViewGroup) view).getChildAt(i), canDo);
             }
         }
     }
@@ -117,15 +126,14 @@ public class ImageCropActivity extends Activity {
         holderImageCrop = findViewById(R.id.holderImageCrop);
         imageView = findViewById(R.id.imageView);
         ivRotate = findViewById(R.id.ivRotate);
-        ivInvert = findViewById(R.id.ivInvert);
         ivRebase = findViewById(R.id.ivRebase);
         btnImageCrop.setText(ScannerConstants.cropText);
         btnClose.setText(ScannerConstants.backText);
         polygonView = findViewById(R.id.polygonView);
         progressBar = findViewById(R.id.progressBar);
-        if (progressBar.getIndeterminateDrawable()!=null && ScannerConstants.progressColor!=null)
+        if (progressBar.getIndeterminateDrawable() != null && ScannerConstants.progressColor != null)
             progressBar.getIndeterminateDrawable().setColorFilter(Color.parseColor(ScannerConstants.progressColor), android.graphics.PorterDuff.Mode.MULTIPLY);
-        else if (progressBar.getProgressDrawable()!=null && ScannerConstants.progressColor!=null)
+        else if (progressBar.getProgressDrawable() != null && ScannerConstants.progressColor != null)
             progressBar.getProgressDrawable().setColorFilter(Color.parseColor(ScannerConstants.progressColor), android.graphics.PorterDuff.Mode.MULTIPLY);
         setProgressBar(true);
         btnImageCrop.setBackgroundColor(Color.parseColor(ScannerConstants.cropColor));
@@ -151,7 +159,7 @@ public class ImageCropActivity extends Activity {
     private void initializeCropping() {
 
         selectedImageBitmap = ScannerConstants.selectedImageBitmap;
-        tempBitmapOrginal=selectedImageBitmap.copy(selectedImageBitmap.getConfig(),true);
+        tempBitmapOrginal = selectedImageBitmap.copy(selectedImageBitmap.getConfig(), true);
         ScannerConstants.selectedImageBitmap = null;
 
         Bitmap scaledBitmap = scaledBitmap(selectedImageBitmap, holderImageCrop.getWidth(), holderImageCrop.getHeight());
@@ -184,7 +192,7 @@ public class ImageCropActivity extends Activity {
             setProgressBar(true);
             Observable.fromCallable(() -> {
                 ScannerConstants.selectedImageBitmap = getCroppedImage();
-                if (ScannerConstants.selectedImageBitmap ==null)
+                if (ScannerConstants.selectedImageBitmap == null)
                     return false;
                 if (ScannerConstants.saveStorage)
                     saveToInternalStorage(ScannerConstants.selectedImageBitmap);
@@ -194,29 +202,22 @@ public class ImageCropActivity extends Activity {
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe((result) -> {
                         setProgressBar(false);
-                        if (ScannerConstants.selectedImageBitmap !=null)
-                        {
+                        if (ScannerConstants.selectedImageBitmap != null) {
                             setResult(RESULT_OK);
                             finish();
                         }
                     });
-
-
         }
     };
 
     private View.OnClickListener btnRebase = v -> {
-        ScannerConstants.selectedImageBitmap=tempBitmapOrginal.copy(tempBitmapOrginal.getConfig(),true);
-        isInverted=false;
+        ScannerConstants.selectedImageBitmap = tempBitmapOrginal.copy(tempBitmapOrginal.getConfig(), true);
         initializeElement();
     };
 
     private View.OnClickListener btnCloseClick = v -> finish();
 
-    private void invertColor()
-    {
-        if (!isInverted)
-        {
+    private void invertColor() {
             Bitmap bmpMonochrome = Bitmap.createBitmap(selectedImageBitmap.getWidth(), selectedImageBitmap.getHeight(), Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(bmpMonochrome);
             ColorMatrix ma = new ColorMatrix();
@@ -224,7 +225,7 @@ public class ImageCropActivity extends Activity {
             Paint paint = new Paint();
             paint.setColorFilter(new ColorMatrixColorFilter(ma));
             canvas.drawBitmap(selectedImageBitmap, 0, 0, paint);
-            selectedImageBitmap=bmpMonochrome.copy(bmpMonochrome.getConfig(),true);
+            selectedImageBitmap = bmpMonochrome.copy(bmpMonochrome.getConfig(), true);
 
             Mat src = new Mat(selectedImageBitmap.getHeight(), selectedImageBitmap.getWidth(), CvType.CV_8UC4);
             Utils.bitmapToMat(selectedImageBitmap, src);
@@ -238,12 +239,6 @@ public class ImageCropActivity extends Activity {
             Utils.matToBitmap(src, processedImage);
 
             selectedImageBitmap = processedImage;
-        }
-        else
-        {
-            selectedImageBitmap=tempBitmapOrginal.copy(tempBitmapOrginal.getConfig(),true);
-        }
-        isInverted=!isInverted;
     }
 
     private View.OnClickListener btnInvertColor = new View.OnClickListener() {
@@ -273,9 +268,7 @@ public class ImageCropActivity extends Activity {
         public void onClick(View v) {
             setProgressBar(true);
             Observable.fromCallable(() -> {
-                if (isInverted)
-                    invertColor();
-                ScannerConstants.selectedImageBitmap=rotateBitmap(selectedImageBitmap,90);
+                ScannerConstants.selectedImageBitmap = rotateBitmap(selectedImageBitmap, 90);
                 return false;
             })
                     .subscribeOn(Schedulers.io())
@@ -286,12 +279,13 @@ public class ImageCropActivity extends Activity {
                     });
         }
     };
-    private String saveToInternalStorage(Bitmap bitmapImage){
+
+    private String saveToInternalStorage(Bitmap bitmapImage) {
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
         File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "cropped_" + timeStamp + ".png";
-        File mypath=new File(directory,imageFileName);
+        File mypath = new File(directory, imageFileName);
         FileOutputStream fos = null;
         try {
             fos = new FileOutputStream(mypath);
@@ -307,8 +301,11 @@ public class ImageCropActivity extends Activity {
         }
         return directory.getAbsolutePath();
     }
+
     protected Bitmap getCroppedImage() {
         try {
+            invertColor();
+
             Map<Integer, PointF> points = polygonView.getPoints();
 
             float xRatio = (float) selectedImageBitmap.getWidth() / imageView.getWidth();
@@ -323,15 +320,14 @@ public class ImageCropActivity extends Activity {
             float y3 = (points.get(2).y) * yRatio;
             float y4 = (points.get(3).y) * yRatio;
             return nativeClass.getScannedBitmap(selectedImageBitmap, x1, y1, x2, y2, x3, y3, x4, y4);
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(ImageCropActivity.this,ScannerConstants.cropError,Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ImageCropActivity.this, ScannerConstants.cropError, Toast.LENGTH_SHORT).show();
                 }
             });
-            return  null;
+            return null;
         }
     }
 
@@ -349,8 +345,8 @@ public class ImageCropActivity extends Activity {
 
     private List<PointF> getContourEdgePoints(Bitmap tempBitmap) {
         MatOfPoint2f point2f = nativeClass.getPoint(tempBitmap);
-        if (point2f==null)
-            point2f= new MatOfPoint2f();
+        if (point2f == null)
+            point2f = new MatOfPoint2f();
         List<Point> points = Arrays.asList(point2f.toArray());
         List<PointF> result = new ArrayList<>();
         for (int i = 0; i < points.size(); i++) {
@@ -377,5 +373,6 @@ public class ImageCropActivity extends Activity {
         }
         return orderedPoints;
     }
+
 
 }
